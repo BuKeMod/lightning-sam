27e4601b8fb06e1ca179b2fbbb5c2f2060ba6d73
BuKeMod <71030661+BuKeMod@users.noreply.github.com>
2023-12-18 15:51:56 +0700

    Update train.py

--- lightning_sam/train.py
+++ lightning_sam/train.py
@@ -162,6 +162,41 @@ def main(cfg: Box) -> None:
     train_sam(cfg, fabric, model, optimizer, scheduler, train_data, val_data)
     validate(fabric, model, val_data, epoch=0)
 
+######
+    config1 = {
+        "num_devices": 2,
+        "batch_size": 12,
+        "num_workers": 4,
+        "num_epochs": 20,
+        "eval_interval": 2,
+        "out_dir": "/kaggle/working/training",
+        "opt": {
+            "learning_rate": 8e-4,
+            "weight_decay": 1e-4,
+            "decay_factor": 10,
+            "steps": [60000, 86666],
+            "warmup_steps": 250,
+        },
+        "model": {
+            "type": 'vit_b',
+            "checkpoint": "/kaggle/working/sam_vit_b_01ec64.pth",
+            "freeze": {
+                "image_encoder": True,
+                "prompt_encoder": True,
+                "mask_decoder": False,
+            },
+        },
+        "dataset": {
+            "train": {
+                "root_dir": "/kaggle/working/Crop-Fields-LOD-13-14-15-4/train",
+                "annotation_file": "/kaggle/working/Crop-Fields-LOD-13-14-15-4/train/sa_Tannotationscoco.json"
+            },
+            "val": {
+                "root_dir": "/kaggle/working/Crop-Fields-LOD-13-14-15-4/valid",
+                "annotation_file": "/kaggle/working/Crop-Fields-LOD-13-14-15-4/valid/sa_Vannotationscoco.json"
+            }
+        }
+    }
 #######################
 if __name__ == "__main__":
     from box import Box
@@ -209,7 +244,7 @@ if __name__ == "__main__":
 
     parser = create_parser()
     args = parser.parse_args()
-    cfg = load_config(args.config_file)
+    cfg = load_config(args.config1)
 
 
     main(cfg)
