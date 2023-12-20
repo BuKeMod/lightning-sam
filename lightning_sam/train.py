1fb82317764fc4dc921866aa0092df1eae3c1183
BuKeMod <71030661+BuKeMod@users.noreply.github.com>
2023-12-20 10:28:04 +0700

    Update train.py

--- lightning_sam/train.py
+++ lightning_sam/train.py
@@ -158,7 +158,7 @@ def main(cfg: Box) -> None:
     fabric = L.Fabric(
                     accelerator="tpu",  # เปลี่ยน accelerator เป็น "auto_tpu" เพื่อให้ Lightning ใช้ TPU อัตโนมัติ
                     devices="auto",  # เปลี่ยน devices เป็น "auto" เพื่อให้ Lightning เลือกจำนวน TPU ที่พร้อมใช้งาน
-                    strategy="auto",
+                    strategy="xla",
                     loggers=[TensorBoardLogger(cfg.out_dir, name="lightning-sam")]
                     )
         
