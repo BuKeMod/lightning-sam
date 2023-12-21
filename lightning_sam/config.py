4edc83aeae945b0f37bdd1cf3987806d7dff5fb0
BuKeMod <71030661+BuKeMod@users.noreply.github.com>
2023-12-21 10:37:49 +0700

    Update config.py

--- lightning_sam/config.py
+++ lightning_sam/config.py
@@ -1,38 +1,38 @@
-# from box import Box
+from box import Box
 
-# config = {
-#     "num_devices": 2,
-#     "batch_size": 12,
-#     "num_workers": 4,
-#     "num_epochs": 20,
-#     "eval_interval": 2,
-#     "out_dir": "/kaggle/working/training",
-#     "opt": {
-#         "learning_rate": 8e-4,
-#         "weight_decay": 1e-4,
-#         "decay_factor": 10,
-#         "steps": [60000, 86666],
-#         "warmup_steps": 250,
-#     },
-#     "model": {
-#         "type": 'vit_b',
-#         "checkpoint": "/kaggle/working/sam_vit_b_01ec64.pth",
-#         "freeze": {
-#             "image_encoder": True,
-#             "prompt_encoder": True,
-#             "mask_decoder": False,
-#         },
-#     },
-#     "dataset": {
-#         "train": {
-#             "root_dir": "/kaggle/working/Crop-Fields-LOD-13-14-15-4/train",
-#             "annotation_file": "/kaggle/working/Crop-Fields-LOD-13-14-15-4/train/sa_Tannotationscoco.json"
-#         },
-#         "val": {
-#             "root_dir": "/kaggle/working/Crop-Fields-LOD-13-14-15-4/valid",
-#             "annotation_file": "/kaggle/working/Crop-Fields-LOD-13-14-15-4/valid/sa_Vannotationscoco.json"
-#         }
-#     }
-# }
+config = {
+    "num_devices": 2,
+    "batch_size": 12,
+    "num_workers": 4,
+    "num_epochs": 20,
+    "eval_interval": 2,
+    "out_dir": "/kaggle/working/training",
+    "opt": {
+        "learning_rate": 8e-4,
+        "weight_decay": 1e-4,
+        "decay_factor": 10,
+        "steps": [60000, 86666],
+        "warmup_steps": 250,
+    },
+    "model": {
+        "type": 'vit_b',
+        "checkpoint": "/kaggle/working/sam_vit_b_01ec64.pth",
+        "freeze": {
+            "image_encoder": True,
+            "prompt_encoder": True,
+            "mask_decoder": False,
+        },
+    },
+    "dataset": {
+        "train": {
+            "root_dir": "/kaggle/working/Crop-Fields-LOD-13-14-15-4/train",
+            "annotation_file": "/kaggle/working/Crop-Fields-LOD-13-14-15-4/train/sa_Tannotationscoco.json"
+        },
+        "val": {
+            "root_dir": "/kaggle/working/Crop-Fields-LOD-13-14-15-4/valid",
+            "annotation_file": "/kaggle/working/Crop-Fields-LOD-13-14-15-4/valid/sa_Vannotationscoco.json"
+        }
+    }
+}
 
-# cfg = Box(config)
+cfg = Box(config)
